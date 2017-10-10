5d2006f4ab92f64bae107b5788db9e8419fdd748
Goborijung <32215618+Goborijung@users.noreply.github.com>
2017-10-10 15:21:18 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,4 +1,5 @@
 <?php
+
 // echo "Hello Bot Goborijung";
 
 $strAccessToken = "OyJ4nQPnUj2MIhEdFLugs9MQMDaRyNglNtwIVwJRPhqu+i1ucjxFz+N2aK2T/88Bf3Xc6MAdEYeUpQGXVxTTVzLPS6yIxY/p8fAvjga33iZGAShng8Au+dTdpctQKg+u1QhZ/6eogkV9D0DXfxQELgdB04t89/1O/w1cDnyilFU=";
@@ -8,45 +9,40 @@ $strUrl = "https://api.line.me/v2/bot/message/reply";
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$_msg = $arrJson['events'][0]['message']['text'];
 
-// Text or Message Onliy
-$_msg = $arrJson['events'][0]['message']['text']; //request api
-$arrPostData = array(); //response api
-$_replyToken = $arrJson['events'][0]['replyToken'];
-$_replyText = $arrPostData['messages'][0]['type'] = "text";
-$_postData = $arrPostData['messages'][0]['text'];
-
-/*
-switch($_msg){
-	case "555" : 
-	$arrPostData['replyToken'] = $_replyToken; $_replyText; $_postData = "666";
-	break;
-
-	case "สวัสดี" :
-	$arrPostData['replyToken'] = $_replyToken; $_replyText; $_postData = "สวัสดี ID คุณคือ " . $arrJson['events'][0]['source']['userId'];
-	break;
-
-	case "ชื่ออะไร" : 
-	$arrPostData['replyToken'] = $_replyToken; $_replyText; $_postData = "ฉันยังไม่มีชื่อนะ";
-	break;
-
-	case "ทำอะไรได้บ้าง" : 
-	$arrPostData['replyToken'] = $_replyToken; $_replyText; $_postData = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-	break;
-
-
-	default: 
-	$arrPostData['replyToken'] = $_replyToken; $_replyText; $_postData = "ฉันไม่เข้าใจคำสั่ง";
-	break;
-}
-*/
-
-if($_msg == "444"){
+if ($_msg == "สวัสดี")
+	{
+	$arrPostData = array();
+	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+	$arrPostData['messages'][0]['type'] = "text";
+	$arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ " . $arrJson['events'][0]['source']['userId'];
+	}
+  else
+if ($_msg == "ชื่ออะไร")
+	{
+	$arrPostData = array();
+	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+	$arrPostData['messages'][0]['type'] = "text";
+	$arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+	}
+  else
+if ($_msg == "ทำอะไรได้บ้าง")
+	{
 	$arrPostData = array();
 	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
 	$arrPostData['messages'][0]['type'] = "text";
 	$arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}
+	}
+  else
+	{
+	/*
+	$arrPostData = array();
+	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+	$arrPostData['messages'][0]['type'] = "text";
+	$arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+	*/
+	}
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $strUrl);
