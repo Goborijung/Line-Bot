d8364ccacd1702bb5f0bfc6403fbb4090e55a2bd
Goborijung <32215618+Goborijung@users.noreply.github.com>
2018-02-21 10:16:45 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,82 +1,50 @@
 <?php
+$access_token = 'OyJ4nQPnUj2MIhEdFLugs9MQMDaRyNglNtwIVwJRPhqu+i1ucjxFz
 
-// echo "Hello Bot Goborijung";
++N2aK2T/88Bf3Xc6MAdEYeUpQGXVxTTVzLPS6yIxY/p8fAvjga33iZGAShng8Au+dTdpctQKg
 
-$strAccessToken = "OyJ4nQPnUj2MIhEdFLugs9MQMDaRyNglNtwIVwJRPhqu+i1ucjxFz+N2aK2T/88Bf3Xc6MAdEYeUpQGXVxTTVzLPS6yIxY/p8fAvjga33iZGAShng8Au+dTdpctQKg+u1QhZ/6eogkV9D0DXfxQELgdB04t89/1O/w1cDnyilFU=";
++u1QhZ/6eogkV9D0DXfxQELgdB04t89/1O/w1cDnyilFU=';
 $content = file_get_contents('php://input');
-$arrJson = json_decode($content, true);
-$strUrl = "https://api.line.me/v2/bot/message/reply";
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+$events = json_decode($content, true);
+if (!is_null($events['events'])) {
+ foreach ($events['events'] as $event) {
 
-$_msg = $arrJson['events'][0]['message']['text'];
+  if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 
-if ($_msg == "สวัสดี")
-	{
-	$arrPostData = array();
-	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	$arrPostData['messages'][0]['type'] = "text";
-	$arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ " . $arrJson['events'][0]['source']['userId'];
-	}
+   $text = $event['message']['text'];
+ 
+   $replyToken = $event['replyToken'];
 
-if ($_msg == "ขอแนวข้อสอบ")
-	{
-	$arrPostData = array();
-	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	$arrPostData['messages'][0]['type'] = "text";
-	$arrPostData['messages'][0]['text'] = "ต้องขออภัย! ตอนนี้ยังไม่มีแนวข้อสอบนะครับผม :) ";
-	}
+  
+   $messages = [
+    'type' => 'text',
+    'text' => $text
+   ];
 
-if ($_msg == "?")
-	{
-	$arrPostData = array();
-	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	$arrPostData['messages'][0]['type'] = "text";
-	$arrPostData['messages'][0]['text'] = "ครับผม ต้องการให้เราช่วยเหลืออะไรครับ ??? <br> 555+";
-	}
 
-if ($_msg == "img")
-	{
-	$arrPostData = array();
-	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	$arrPostData['messages'][0]['type'] = "image";
-	$arrPostData['messages'][0]['originalContentUrl'] = "https://www.ez-miner.com/Img/landing/avatar3.jpg";
-	$arrPostData['messages'][0]['previewImageUrl'] = "https://www.ez-miner.com/Img/landing/avatar3.jpg";
-	}
+   $url = 'https://api.line.me/v2/bot/message/reply';
+   $data = [
+    'replyToken' => $replyToken,
+    'messages' => [$messages],
+   ];
+   $post = json_encode($data);
+   $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . 
 
-if ($_msg == "ตารางสอบ")
-	{
-	$arrPostData = array();
-	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	$arrPostData['messages'][0]['type'] = "text";
-	$arrPostData['messages'][0]['text'] = "ต้องขออภัย! ตอนนี้ยังไม่มี ตารางสอบนะครับผม :) ";
-	}
+$access_token);
 
-if ($_msg == "ขอบคุณครับ")
-	{
-	$arrPostData = array();
-	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	$arrPostData['messages'][0]['type'] = "text";
-	$arrPostData['messages'][0]['text'] = "ครับผม :)";
-	}
+   $ch = curl_init($url);
+   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+   $result = curl_exec($ch);
+   curl_close($ch);
 
-if ($_msg == "ทำอะไรได้บ้าง")
-	{
-	$arrPostData = array();
-	$arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-	$arrPostData['messages'][0]['type'] = "text";
-	$arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-	}
+   echo $result . "\r\n";
+  }
+ }
+}
+echo "OK";
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close($ch);
 ?>
