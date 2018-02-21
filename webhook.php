17b14d6223b93b56c7a36f9969a51e0194245b86
Goborijung <32215618+Goborijung@users.noreply.github.com>
2018-02-21 15:25:36 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -11,7 +11,7 @@ if (!is_null($events['events'])) {
    $replyToken = $event['replyToken'];
    
    if($text == '?'){
-    $messages = ['type' => 'text', 'text' => 'ต้องการให้เราช่วยอะไรคับ ?<br/>xx'];
+    $messages = ['type' => 'text', 'text' => 'ต้องการให้เราช่วยอะไรคับ ? \r\n /r/n <br> <br/> gogo'];
    }else{
     $messages = ['type' => 'text', 'text' => $text];
    }
